87f4338579cf07c17b829fcae5ffc126c1b419b6
Rahul Madhavan <rahul.maddy@gmail.com>
2022-07-25 11:12:49 +0530

    content changes

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 :: Type the command "cmd < start.sh"
 :: Preliminary Checks:
-:: 1. Ensure that you are on the IISc CISCO VPN
-:: 2. Ensure that the remote ssh credentials are added to github repo
+:: 1. Ensure that the remote ssh credentials are added to github repo
 
 :: Now Git changes
 git remote add origin git@github.com:p10rahulm/AlgosUnderUncertainty.git
