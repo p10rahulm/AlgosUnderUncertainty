cab0fd511600163a2333875b88f7880026e87a05
Rahul Madhavan <rahul.maddy@gmail.com>
2022-08-03 18:07:36 +0530

    content changes

--- sync.sh
+++ sync.sh
@@ -4,3 +4,4 @@ git pull origin main
 cp index.html /home/barman/public_html/AlgUncertain/index.html
 cp styles.css /home/barman/public_html/AlgUncertain/styles.css
 cp -R images /home/barman/public_html/AlgUncertain/
+cp -R static /home/barman/public_html/AlgUncertain/
